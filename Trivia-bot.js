c5552eaca002d332d0de8967ed25a166becb6be0
Ayush Bisht <ayushbisht@gmail.com>
2026-03-01 11:24:41 +0530

    Added new Trivia bot

---
+++ Trivia-bot.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+// Bot details
+let botName = "CodeBuddy";
+let botLocation = "the Internet";
+let favoriteLanguage = "JavaScript";
+
+// Introduction
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// First fun fact
+let codingFact = favoriteLanguage + " was created in just 10 days!";
+console.log(codingFact);
+
+// Second fun fact
+codingFact = favoriteLanguage + " is one of the most popular programming languages in the world.";
+console.log(codingFact);
+
+// Third fun fact
+codingFact = favoriteLanguage + " can be used for both frontend and backend development.";
+console.log(codingFact);
+
+// Farewell message
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
