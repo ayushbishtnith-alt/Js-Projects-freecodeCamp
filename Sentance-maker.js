5863bbcae735743496233a5a3c30860d5f629b52
Ayush Bisht <ayushbisht@gmail.com>
2026-03-01 12:00:09 +0530

    Now added new Project

---
+++ Sentance-maker.js
@@ -0,0 +1,25 @@
+// Declare variables
+let adjective = "fierce";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountain";
+let adjective2 = "smoky";
+let noun2 = "marshmallows";
+
+// First Story
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+
+adjective = "tiny";
+noun = "unicorn";
+verb = "excited";
+place = "forest";
+adjective2 = "sparkly";
+noun2 = "cupcakes";
+
+// Second Story
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
